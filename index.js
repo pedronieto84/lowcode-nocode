74f23f599c522f4d9eeadf67bf2e06ed18060b81
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-11-28 16:59:57 +0100

    Exercici básic de agafar dades d'un array d'objectes JSON.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Ejemplo de objeto JSON de tipo usuario esta en el archivo user.json
+
+var users = require('./user.json')
+
+
+// Saber la edad de pedro.
+
+var edadPedro = users[0].edad 
+console.log('edad de pedro es', edadPedro)
+
+
+// Saber el nombre del segundo usuario.
+var nombreSegundoUsuario = users[1].edad
+console.log('nombre segundo usuario', nombreSegundoUsuario)
+
+// Saber el nom de el segon/a fill/a de Laura
+var nombreSegundoFillLaura = users[1].fills[1]
+console.log('Nombre segona filla de Laura', nombreSegundoFillLaura)
