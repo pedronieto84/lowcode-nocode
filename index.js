c35e39a02e210554805bd85020e67d2d27d7a526
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-11-28 16:54:17 +0100

    Tipos de datos. Number, null, undefined, boolean, object, array.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // String
 var nombre = 'Pedro'
+
+// Number
+var numero = 243
+
+// Boolean
+var booleano = true
+
+// null
+var nulo = null
+
+// Undefined
+var nodefinido = Undefined
+
+// Object
+var objecte = {
+    nombre: "lowcode",
+    tipo: "curso"
+}
+
+// Array
+var array = ["Xavier", "Jose Antonio", "Maria Jose", "Ana", "..."]
