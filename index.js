ad9825fac6bd98130af72420c754d4e4336cba3a
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-11-29 20:01:11 +0100

    Quito el Hello WOrld

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ let users = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 // OPERACIONS CRUD
 
 // OPERACIO READ ALL USERS
@@ -40,62 +36,51 @@ app.get("/user/:id", (req, res) => {
 app.post("/user", (req, res) => {
   const user = req.body;
   console.log("user", user);
-  users.push(user)
-  res.send( { result: 'success'} );
+  users.push(user);
+  res.send({ result: "success" });
 });
 
-
 // OPERACIO UPDATE ONE USER AMB METODE PUT
-app.put("/user/:id", (req, res)=>{
-    // Primero tengo que coger la id del usuario que quiero updatear
-    const id = req.params.id
+app.put("/user/:id", (req, res) => {
+  // Primero tengo que coger la id del usuario que quiero updatear
+  const id = req.params.id;
 
-    // Segundo tengo que coger el object json con la data que quiero modificar
-    const userToUpdate = req.body
+  // Segundo tengo que coger el object json con la data que quiero modificar
+  const userToUpdate = req.body;
 
-    // Tengo que encontrar el elemento del array y substituirlo por el nuevo objeto que me viene del body
-    users[id] = userToUpdate
+  // Tengo que encontrar el elemento del array y substituirlo por el nuevo objeto que me viene del body
+  users[id] = userToUpdate;
 
-    res.send({result: 'success'})
-})
-
+  res.send({ result: "success" });
+});
 
 // OPERACIO UPDATE ONE USER AMB METODE PATCH
-app.patch("/user/:id", (req, res)=>{
+app.patch("/user/:id", (req, res) => {
+  // Primero tengo que coger la id del usuario que quiero updatear
+  const id = req.params.id;
 
-    // Primero tengo que coger la id del usuario que quiero updatear
-    const id = req.params.id
+  // Segundo tengo que coger el object json con la data que quiero modificar
+  const userToUpdate = req.body;
+  const keys = Object.keys(userToUpdate)[0];
 
-    // Segundo tengo que coger el object json con la data que quiero modificar
-    const userToUpdate = req.body
-    const keys = Object.keys(userToUpdate)[0]
+  // Primero cojo el objeto que habia antes
+  const objectePrevi = users[id];
 
-    // Primero cojo el objeto que habia antes
-    const objectePrevi = users[id]
+  objectePrevi[keys] = Object.values(userToUpdate)[0];
+  // Si hay alguna propiedad nueva
 
-    objectePrevi[keys] = Object.values(userToUpdate)[0]
-    // Si hay alguna propiedad nueva
-    
-   
-    
-
-    res.send({result: 'success'})
-})
-
-
+  res.send({ result: "success" });
+});
 
 // OPERACIO DELETE ONE USER
 app.delete("/user/:id", (req, res) => {
-    const id = req.params.id
-    users[id] = null
-    users = users.filter((user)=>{
-        return user
-    })
-    res.send({result: 'success'})
+  const id = req.params.id;
+  users[id] = null;
+  users = users.filter((user) => {
+    return user;
+  });
+  res.send({ result: "success" });
 });
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
