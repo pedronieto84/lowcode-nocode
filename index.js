683ee9eac488b36185d84ef228ba8f6cb129d180
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-11-29 19:00:16 +0100

    Creo array de Usuarios con los que trabajar.

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let users = [{
+    nombre: 'Pedro'
+    },
+    {
+        nombre: 'Laura'
+    },
+    {
+        nombre: 'Lluis'
+    }
+]
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
