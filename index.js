97839ca4b8d0a3d712c50a1e0179412e11ad0843
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-11-29 17:58:15 +0100

    Boilerplate de Api CRUD con Express

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 console.log('hola mundo')
 console.log('Segundo commit')
-console.log('Tercer Commit')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
