e92f9fdfa7972bb6a7583abec9ea793a45f7cc09
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-11-29 19:31:00 +0100

    Creo peticiones de Get Users, Get un user y post User

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-console.log('hola mundo')
-console.log('Segundo commit')
-const express = require('express')
-const app = express()
-const port = 3000
+console.log("hola mundo");
+console.log("Segundo commit");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+const port = 3000;
 
 let users = [
-    {
-        nombre: 'Pedro'
-    }
-    ,
-    {
-        nombre: 'Laura'
-    }
-    ,
-    {
-        nombre: 'Lluis'
-    }
-]
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  {
+    nombre: "Pedro",
+  },
+  {
+    nombre: "Laura",
+  },
+  {
+    nombre: "Lluis",
+  },
+];
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+// OPERACIONS CRUD
+
+// OPERACIO READ ALL USERS
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+// OPERACIO READ ONE USER
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("id", id);
+  res.send(users[id]);
+});
+
+// OPERACIO CREATE ONE USER
+app.post("/user", (req, res) => {
+  const user = req.body;
+  console.log("user", user);
+  users.push(user)
+  res.send( { result: 'success'} );
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
