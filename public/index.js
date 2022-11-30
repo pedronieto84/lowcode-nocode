a5de0f4e1e28f27a8245593663da2404afb4877e
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-11-30 19:35:49 +0100

    Preparo el index.js de public

---
+++ public/index.js
@@ -0,0 +1,50 @@
+
+fetch('https://firestore.googleapis.com/v1/projects/eliminarlo2/databases/(default)/documents/usuarios')
+  .then(response => response.json())
+  .then(data => {
+    console.log('data', data)
+    console.log('users', users)
+    return users
+  })
+  .then((users)=>{
+    console.log('users', users)
+    addTitle()
+    addList(users)
+  })
+
+function addTitle() {
+    
+  // Creo un DIV element
+  const newDiv = document.createElement("div");
+
+  // Le pongo un título
+  const newContent = document.createTextNode("Listado de Usuarios");
+  newDiv.appendChild(newContent);
+
+  // Lo inserto al DOM
+  const currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
+
+function addList(users){
+  // Creo un elemento UL
+  const ul = document.createElement("ul");
+
+  // Itero sobre cada usuario
+  users.forEach((user)=>{
+
+    // En cada usuario creo un elemento LI y le añado el nombre
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(user.nombre));
+
+    // Añado el elemento LI al elemento UL
+    ul.appendChild(li);
+  })
+  
+
+  // Lo inserto al DOM
+  const currentDiv = document.getElementById("div1");
+  document.body.insertBefore(ul, currentDiv);
+}
+
+
