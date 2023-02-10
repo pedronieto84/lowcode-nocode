0e725d5115607b802e1e9797909faf81dec20f3f
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2023-02-10 15:50:44 +0100

    Ejemplo de ordenar array de objetos basados en una propiedad de tipo string

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ var users = require('./user.json')
 
 // Metodo sort para que considere correctamente los números y no se quede solamente con el metodo por defecto que los interpreta como si fueran strings.
 
-users.sort((previo, posterior)=> previo.edad - posterior.edad)
+users.sort((previo, posterior)=> {
+let x = previo.nombre.toLowerCase();
+let y = posterior.nombre.toLowerCase();
+if (x < y) {return -1;}
+if (x > y) {return 1;}
+return 0;
+})
 
 console.log('ordenados por edad', users)
 
