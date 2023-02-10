4f9648b7eeb32298d637fa7e205e763703f08385
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2023-02-10 16:00:04 +0100

    Metodo Map, modifica la estructura de cada objeto de dentro del array.

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ var users = require('./user.json')
 
 
 
-// Metodo ForEach itera por cada elemento y dentro de la callback function pues determino que hacer
-let edadTotal = 0
-users.forEach((user, index, arrayTotal)=>{
-    console.log('user', user)
-    console.log('index', index)
-    console.log('array en ese preciso instante', arrayTotal)
-    edadTotal = edadTotal + user.edad
+// Metodo map modifica los elementos del array
+
+users.map((user)=>{
+    user.mayorEdad = user.edad > 18 ? true : false
+    delete user.fills
 })
 
-console.log('edad total', edadTotal)
+console.log('edad total', users)
 
 
 
