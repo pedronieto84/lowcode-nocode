cea0b3b9dc9aef00cd635d5a5b9d0b1e70ec0299
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2023-02-10 15:48:23 +0100

    Metodo para ordenar array de objetos.

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 var users = require('./user.json')
 
-var arrayNumbers = [1,  3, 23, 5, 0, -1, 2]
+
 
 // Metodo sort para que considere correctamente los números y no se quede solamente con el metodo por defecto que los interpreta como si fueran strings.
 
-arrayNumbers.sort((a, b) => a-b)
+users.sort((previo, posterior)=> previo.edad - posterior.edad)
 
-console.log('arrayNumbers', arrayNumbers)
+console.log('ordenados por edad', users)
 
 
 
