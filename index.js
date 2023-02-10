3c28ff9f0e0140bb16efffe00e87eac395237001
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2023-02-10 15:38:09 +0100

    Metodo sort para que ordene numeros considerando que son numeros y no son strings.

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var users = require('./user.json')
 
 var arrayNumbers = [1,  3, 23, 5, 0, -1, 2]
 
-// Metodo sort
+// Metodo sort para que considere correctamente los números y no se quede solamente con el metodo por defecto que los interpreta como si fueran strings.
 
-arrayNumbers.sort()
+arrayNumbers.sort((a, b) => a-b)
 
 console.log('arrayNumbers', arrayNumbers)
 
